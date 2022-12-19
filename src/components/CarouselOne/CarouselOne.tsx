443d55f1b38bc4849d3f7bf02e85da0e3f083901
HuldaCZ <phulinsky@gmail.com>
2022-12-20 00:28:29 +0100

    Arrow buttons

--- src/components/CarouselOne/CarouselOne.tsx
+++ src/components/CarouselOne/CarouselOne.tsx
@@ -1,5 +1,8 @@
+import classNames from 'classnames';
 import { useGetUsersCollectionsQuery } from '../../api/users';
 import { Carousel } from '../../lib';
+import { ReactComponent as ArrowRight } from '../../icons/arrow-right.svg';
+import { ReactComponent as ArrowLeft } from '../../icons/arrow-left.svg';
 
 import styles from './CarouselOne.module.scss';
 
@@ -9,15 +12,25 @@ const CarouselOne = (): JSX.Element => {
     <Carousel data={usersCollectionData || []}>
       <Carousel.CarouselView className={styles.carousel_view}>
         <>
-          <Carousel.ArrowButton side="left" className={styles.button_left} />
-          <Carousel.ArrowButton side="right" className={styles.button_right} />
+          <Carousel.ArrowButton
+            side="left"
+            className={classNames(styles.button, styles.button_left)}
+          >
+            <ArrowLeft />
+          </Carousel.ArrowButton>
+          <Carousel.ArrowButton
+            side="right"
+            className={classNames(styles.button, styles.button_right)}
+          >
+            <ArrowRight />
+          </Carousel.ArrowButton>
           {usersCollectionData?.map(({ image, title }, index) => (
             <Carousel.CarouselItem
               key={index}
               index={index}
               className={styles.carousel_item}
               {...{ image }}
-              animation='zoomIn'
+              animation="zoomIn"
               translateRatio={50}
             >
               <h2>{title}</h2>
